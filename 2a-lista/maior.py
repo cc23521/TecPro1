2c55e1920512569729f8c43270eb9fce49bcee03
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2023-03-24 11:08:48 -0300

    Remove 2a lista repetida

--- 2a-lista/maior.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Insira um inteiro: "))
-num2 = int(input("Insira outro inteiro: "))
-
-if (num1 > num2):
-    print(f"{num1} é maior que {num2}")
-else:
-    print(f"{num2} é maior que {num1}")
-
--- 2a-lista/menor.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Insira um inteiro: "))
-num2 = int(input("Insira outro inteiro: "))
-
-if (num1 > num2):
-    print(f"{num2} é menor que {num1}")
-else:
-    print(f"{num1} é menor que {num2}")
-
