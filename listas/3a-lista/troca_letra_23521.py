900293012fc30d2018b998a5e93fb4e1556f4dfe
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2023-03-25 23:02:35 -0300

    Adiciona cabeçalho

--- listas/3a-lista/troca_letra_23521.py
+++ listas/3a-lista/troca_letra_23521.py
@@ -1,3 +1,13 @@
+"""
+Curso: DesenvSistemas59
+Disciplina: Técnicas de Programação I
+Professora: Andréia
+Aluno: Gustavo Inacio de Oliveira Cruz
+RA: 23521
+Lista: 3a-lista
+Data: 27/03/2023
+"""
+
 def troca_letra(texto, letra_alvo, letra_sub):
     """
     Substitui as ocorrências de uma letra alvo de um texto por outra.
