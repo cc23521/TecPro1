ca1ff05e1940536a8e4e1842b8c6ee2bc9132e9c
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2023-04-15 18:32:49 -0300

    Resolve 3a questão da 4a lista

---
+++ listas/vetores/lista1/ex03_23521.py
@@ -0,0 +1,34 @@
+def preenche_vetor(n):
+    """
+    Preenche um vetor de 5 posições com números aleatórios de 0 a n.
+    """
+    from random import randint
+
+    vetor = [0] * 5
+    for i in enumerate(vetor):
+        vetor[i[0]] = randint(0, n)
+    return vetor
+
+def inverte_vetor(vetor):
+    print("Vetor original:", vetor)
+    indice_esquerda = 0
+    indice_direita = len(vetor) - 1
+    while indice_esquerda < indice_direita:
+        # Guarda valores antagonistas do vetor
+        valor_esquerda = vetor[indice_esquerda]
+        valor_direita = vetor[indice_direita]
+        # Troca valores de posição
+        vetor[indice_esquerda] = valor_direita
+        vetor[indice_direita] = valor_esquerda
+        # Atualiza índices
+        indice_esquerda += 1
+        indice_direita -= 1
+    print("Vetor invertido:", vetor)
+
+def solucao():
+    """
+    Resolve terceira questão da lista.
+    """
+    maximo = int(input("Qual o valor de n? "))
+    inverte_vetor(preenche_vetor(maximo))
+
