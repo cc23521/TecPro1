a080228a0d350ce734efd2afd9551c880315a07e
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2023-04-15 18:33:08 -0300

    Resolve 4a questão da 4a lista

---
+++ listas/vetores/lista1/ex04_23521.py
@@ -0,0 +1,52 @@
+def preenche_vetor():
+    """
+    Inputa números do usuário até que seja passado um número negativo.
+    """
+    vetor = []
+    numero = int(input("Insira um número natural: "))
+    while numero >= 0:
+        vetor.append(numero)
+        numero = int(input("Insira um número natural: "))
+    return vetor
+
+def maior_que_cinco(vetor):
+    """
+    Determina quantidade de valores maiores que cinco em <vetor>.
+    """
+    quantidade = 0
+    for i in vetor:
+        if i > 5:
+            quantidade += 1
+    return quantidade
+
+def soma_pares(vetor):
+    """
+    Soma números pares de <vetor>.
+    """
+    soma = 0
+    for i in vetor:
+        if i % 2 == 0:
+            soma += i
+    return soma
+
+def soma_impares(vetor):
+    """
+    Soma números ímpares de <vetor>.
+    """
+    soma = 0
+    for i in vetor:
+        if i % 2 != 0:
+            soma += i
+    return soma
+
+def solucao():
+    """
+    Resolve quarta questão da lista.
+    """
+    vetor = preenche_vetor()
+    print("\nVetor:", vetor)
+    print("Quantidade de valores maiores do que 5:", maior_que_cinco(vetor))
+    print("Soma dos números pares:", soma_pares(vetor))
+    print("Soma dos números ímpares:", soma_impares(vetor))
+    print("Quantidade total de valores armazenados:", len(vetor))
+
